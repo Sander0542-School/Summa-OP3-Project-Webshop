c0d0f18b9b97782e6711c3f95bce12714b044784
Sander Jochems <sanderjochems@hotmail.nl>
2018-03-16 12:18:05 +0100

    Update Handleiding.docx

--- Website/assets/classes/database.php
+++ Website/assets/classes/database.php
@@ -12,7 +12,7 @@ class Database
 	{
     $this->conn = null;
     try {
-      $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name.";charset=utf8;", $this->username, $this->password);
+      $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name.";charset=utf8mb4;", $this->username, $this->password);
 			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $exception) {
       exit("Connection Error: " . $exception->getMessage());
