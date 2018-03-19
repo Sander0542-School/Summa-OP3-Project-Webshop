8a9346aa7f11a0a9d74e6aa96ecc9d4091a5762d
Sander Jochems <sanderjochems@hotmail.nl>
2018-03-19 16:30:02 +0100

    Update core.php

---
+++ Website/assets/classes/core.php
@@ -0,0 +1,123 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once 'database.php';
+
+class CORE
+{
+
+	private $conn;
+
+	public function __construct()
+	{
+		$database = new Database();
+		$db = $database->dbConnection();
+		$this->conn = $db;
+  }
+
+	public function runQuery($sql)
+	{
+		$stmt = $this->conn->prepare($sql);
+		return $stmt;
+	}
+
+	public function lastID()
+	{
+		$stmt = $this->conn->lastInsertId();
+		return $stmt;
+	}
+
+	public function isIngelogd()
+	{
+		if(isset($_SESSION['userSession']))
+		{
+			$stmt = $this->conn->prepare("SELECT * FROM gebruikers WHERE id=:id");
+			$stmt->execute(array(":id"=>$_SESSION['userSession']));
+			$userRow = $stmt->fetch(PDO::FETCH_ASSOC);
+			if ($stmt->rowCount() == 1) {
+				return true;
+			} else {
+				$this->loguit();
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	public function redirect($url)
+	{
+		header("Location: $url");
+	}
+
+	public function logout()
+	{
+		session_unset();
+		session_destroy();
+		$_SESSION['userSession'] = false;
+  }
+  
+  public function register($email, $firstname, $lastname, $password, $phoneNumber, $city, $zipcode, $street, $houseNumber) 
+  {
+    $stmt = $this->conn->prepare("INSERT INTO users (email, firstname, lastname, password, phoneNumber, city, zipcode, street, houseNumber) VALUES(:email, :firstname, :lastname, :password, :phoneNumber, :city, :zipcode, :street, :houseNumber);");
+    $stmt->bindparam(":email",$email);
+    $stmt->bindparam(":firstname",$firstname);
+    $stmt->bindparam(":lastname",$lastname);
+    $stmt->bindparam(":password",$password);
+    $stmt->bindparam(":phoneNumber",$phoneNumber);
+    $stmt->bindparam(":city",$city);
+    $stmt->bindparam(":zipcode",$zipcode);
+    $stmt->bindparam(":street",$street);
+    $stmt->bindparam(":houseNumber",$houseNumberme);
+    $stmt->execute();
+
+    $_SESSION['userSession'] = $this->lastID();
+  }
+
+	public function login($email, $password)
+	{
+		$stmt = $this->conn->prepare("SELECT id FROM customers WHERE email=:email AND password=:password;");
+		$stmt->bindparam(":email",$email);
+		$stmt->bindparam(":password",$password);
+		$stmt->execute();
+		$PS = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		if ($stmt->rowCount() != 1) {
+			return false;
+		} else {
+      $_SESSION['userSession'] = $PS[0]["id"];
+			return true;
+		}
+	}
+
+	public function getBikes($order = 1) {
+    switch($order) {
+      case 1:
+        $orderBy = "name ASC";
+        break;
+      case 2:
+        $orderBy = "name DESC";
+        break;
+      case 3:
+        $orderBy = "price ASC";
+        break;
+      case 4:
+        $orderBy = "price DESC";
+        break;
+      default:
+        $orderBy = "name ASC";
+        break;
+    }
+		$stmt = $this->conn->prepare("SELECT * FROM products ORDER BY ".$orderBy);
+		$stmt->execute();
+		$PS = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		if ($stmt->rowCount() == 0) {
+			return false;
+		} else {
+			return $PS;
+		}
+	}
+}
+
+?>
