4d7fb90a243771d1ed76fc4e50b320e4242bc636
Sander Jochems <sanderjochems@hotmail.nl>
2018-03-19 16:30:09 +0100

    Update fietsen.php

--- Website/fietsen.php
+++ Website/fietsen.php
@@ -3,6 +3,29 @@ $pageTitle = "Fietsen";
 include "assets/header.php";
 ?>
 
+<div class="row">
+  <div class="col col1"></div>
+  <div class="col col8">
+<?php
+    $bikesList = $CORE->getBikes();
+    if ($bikesList) {
+      foreach ($bikesList as $bike) {
+        echo '
+    <div class="card">
+      <div class="card-image">
+        <span></span><img src="'.$bike["imagePath"].'">
+      </div>
+      <div class="card-content">
+        <h2>'.$bike["name"].'</h2>
+        <span>'.$bike["brand"].'</span>
+      </div>
+    </div>';
+      }
+    }
+?>
+  </div>
+</div>
+
 <?php
 include "assets/footer.php"
 ?>
