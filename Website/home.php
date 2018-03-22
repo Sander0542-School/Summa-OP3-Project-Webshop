5fccff2112f9b512cf08c3512b904aab4e2037ac
Sander Jochems <sanderjochems@hotmail.nl>
2018-03-22 11:26:39 +0100

    Update home.php

--- Website/home.php
+++ Website/home.php
@@ -3,6 +3,20 @@ $pageTitle = "Home";
 include "assets/header.php";
 ?>
 
+    <div class="row home margin-top">
+      <div class="col col1"></div>
+      <div class="col col3">
+        <h2>Welkom op de website van De Concurrent</h2>
+        <p>Wij verhuren fietsen aan particulieren en leveren desgewenst bij hotels, camping en B&B in en rondom Nunteren.</p>
+        <p>Tevens kunt u bij ons terecht voor groepsarrangementen, voor u familie, bedrijf of vrienden – uitje.</p>
+        <p>U krijgt bij ons gratis fietsroutes die gemaakt zijn op het knooppunten netwerk.</p>
+      </div>
+      <div class="col col0-5"></div>
+      <div class="col col4-5">
+        <img class="img-homepage" src="/assets/images/concurrent1.jpg" alt="De Concurrent"/>
+      </div>
+    </div>
+
 <?php
 include "assets/footer.php"
 ?>
