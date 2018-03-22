a2a828933f6af78aea964f84c508b1f9f2506d94
Sander Jochems <sanderjochems@hotmail.nl>
2018-03-22 11:27:04 +0100

    Update content.php

---
+++ Website/assets/footer/content.php
@@ -0,0 +1,30 @@
+    <div id="footer" class="footer">
+      <div class="row">
+        <div class="col col0-5"></div>
+        <div class="col col2-5">
+          <img src="/assets/images/logo2.png">
+        </div>
+        <div class="col col4"></div>
+        <div class="col col1">
+          <ul>
+            <li><a href="/home">Home</a></li>
+            <li><a href="/fietsen">Fietsen</a></li>
+            <li><a href="/reparatie">Reparatie</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li><a href="/overons">Over Ons</a></li>
+          </ul>
+        </div>
+        <div class="col col1">
+          <ul>
+            <li><a href="/login">Login</a></li>
+            <li><a href="/registreer">Registreer</a></li>
+          </ul>
+        </div>
+        <div class="col col1"></div>
+      </div>
+      <div class="row footer-dark">
+        <div class="col col10">
+          <p class="copyright">Copyright De Concurrent <?php echo date("Y"); ?> | All Rights Reserved</p>
+        </div>
+      </div>
+    </div>
