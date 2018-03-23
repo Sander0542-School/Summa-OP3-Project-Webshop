52f57765916edcfbb0150194386ddd88f3b28726
Sander Jochems <sanderjochems@hotmail.nl>
2018-03-23 15:36:13 +0100

    Beutify nav.php

--- Website/assets/head/nav.php
+++ Website/assets/head/nav.php
@@ -1,12 +1,12 @@
-<nav>
-  <div class="navbar-left"></div>
-  <a href="#"><img class="nav-logo" alt="De Concurrent" src="/assets/images/logo.png"></a>
-  <a href="/home" class="nav-item<?php if ($pageTitle == "Home") { echo " active"; } ?>">Home</a>
-  <a href="/fietsen" class="nav-item<?php if ($pageTitle == "Fietsen") { echo " active"; } ?>">Fietsen</a>
-  <a href="/reparatie" class="nav-item<?php if ($pageTitle == "Reparatie") { echo " active"; } ?>">Reparatie</a>
-  <a href="/contact" class="nav-item<?php if ($pageTitle == "Contact") { echo " active"; } ?>">Contact</a>
-  <div class="navbar-right"></div>
-  <a href="/login" class="nav-item right<?php if ($pageTitle == "Login") { echo " active"; } ?>">Login</a>
-  <a href="/registreer" class="nav-item right<?php if ($pageTitle == "Registreer") { echo " active"; } ?>">Registreer</a>
-</nav>
-<div class="navbar-top"></div>
+    <nav>
+      <div class="navbar-left"></div>
+      <a href="#"><img class="nav-logo" alt="De Concurrent" src="/assets/images/logo.png"></a>
+      <a href="/home" class="nav-item<?php if ($pageTitle == "Home") { echo " active"; } ?>">Home</a>
+      <a href="/fietsen" class="nav-item<?php if ($pageTitle == "Fietsen") { echo " active"; } ?>">Fietsen</a>
+      <a href="/reparatie" class="nav-item<?php if ($pageTitle == "Reparatie") { echo " active"; } ?>">Reparatie</a>
+      <a href="/contact" class="nav-item<?php if ($pageTitle == "Contact") { echo " active"; } ?>">Contact</a>
+      <div class="navbar-right"></div>
+      <a href="/login" class="nav-item right<?php if ($pageTitle == "Login") { echo " active"; } ?>">Login</a>
+      <a href="/registreer" class="nav-item right<?php if ($pageTitle == "Registreer") { echo " active"; } ?>">Registreer</a>
+    </nav>
+    <div class="navbar-top"></div>
