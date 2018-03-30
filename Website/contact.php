27f64900b6413ae3b7433de6fa6e2e44ddd571c4
Sander Jochems <sanderjochems@hotmail.nl>
2018-03-30 17:29:38 +0200

    Update contact.php

--- Website/contact.php
+++ Website/contact.php
@@ -10,6 +10,34 @@ include "assets/header.php";
       </div>
     </div>
 
+    <div class="row">
+      <div class="col col1"></div>
+      <div class="col col4">
+        <p>De Concurrent</p>
+        <p>Seinpoststraat 8<br/>7766 BG Nunteren<br/>Tel: <a href="tel:+318082001833">0808-2001833</a><br/>Mobiel: <a href="tel:+31693447567">06-93447567</a></p>
+      </div>
+      <div class="col col4">
+        <div id="map" class="google-maps"></div>
+
+        <script>
+          function initMap() {
+            var uluru = {lat: 52.1070807, lng: 4.2824765};
+            var map = new google.maps.Map(document.getElementById('map'), {
+              zoom: 18,
+              center: uluru,
+              zoomControl: false,
+              draggable: false
+            });
+            var marker = new google.maps.Marker({
+              position: uluru,
+              map: map
+            });
+          }
+        </script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDhUaFv5qwATzUG_DlgxbNCH1wXBa-B-PQ&callback=initMap"></script>
+      </div>
+    </div>
+
 <?php
 include "assets/footer.php"
 ?>
