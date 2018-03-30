2b4aba7f070eea8a7d3d13e5794628f4fad75fb7
Sander Jochems <sanderjochems@hotmail.nl>
2018-03-30 17:30:47 +0200

    Update overons.php

--- Website/overons.php
+++ Website/overons.php
@@ -10,6 +10,17 @@ include "assets/header.php";
       </div>
     </div>
 
+    <div class="row">
+      <div class="col col1"></div>
+      <div class="col col3">
+        <p class="about-us">Wij vinden een goed en deskundig advies erg belangrijk bij de aanschaf van een (elektrische) fiets. Komt u daarom gerust eens langs in onze fietsenwinkel in Hardenberg om vrijblijvend te informeren naar de vele mogelijkheden. Een fiets moet bij uw fietsgedrag passen en goed afgestemd zijn op uw lichaam. Natuurlijk mag u ook contact met ons opnemen voor meer informatie. Dit kan via het contactformulier of rechtstreeks door te bellen naar <a href="tel:+318082001833">0808-2001833</a> of een email te sturen naar <a href="mailto:info@deconcurrent.nu">info@deconcurrent.nu</a>.<br/><br/>Wij zijn u graag van dienst!</p>
+      </div>
+      <div class="col col0-5"></div>
+      <div class="col col4-5">
+        <img class="img-homepage" src="/assets/images/concurrent2.jpg" alt="De Concurrent"/><br/>
+      </div>
+    </div>
+
 <?php
 include "assets/footer.php"
 ?>
