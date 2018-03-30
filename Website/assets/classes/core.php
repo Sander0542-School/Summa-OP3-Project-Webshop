fbe346ea4492dd2c8f151bcd59114f2cba404fd6
Sander Jochems <sanderjochems@hotmail.nl>
2018-03-30 17:30:53 +0200

    Update core.php

--- Website/assets/classes/core.php
+++ Website/assets/classes/core.php
@@ -62,7 +62,7 @@ class CORE
   public function register($email, $firstname, $lastname, $password, $phoneNumber, $city, $zipcode, $street, $houseNumber) 
   {
 		try {
-			$stmt = $this->conn->prepare("INSERT INTO customers (email, firstname, lastname, password, phoneNumber, city, zipcode, street, houseNumber) VALUES(:email, :firstname, :lastname, :password, :phoneNumber, :city, :zipcode, :street, :houseNumber);");
+			$stmt = $this->conn->prepare("INSERT INTO customers (email, firstname, lastname, password, phoneNumber, city, zipcode, street, houseNumber) VALUES (:email, :firstname, :lastname, :password, :phoneNumber, :city, :zipcode, :street, :houseNumber);");
 			$stmt->bindparam(":email",$email);
 			$stmt->bindparam(":firstname",$firstname);
 			$stmt->bindparam(":lastname",$lastname);
@@ -133,6 +133,137 @@ class CORE
         break;
     }
 		$stmt = $this->conn->prepare("SELECT id, name, brand, colors, imagePath, model, price, actionPrice, IF(actionPrice IS NULL, false, true) as isAction FROM products ORDER BY ".$orderBy);
+		$stmt->execute();
+		$PS = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		if ($stmt->rowCount() == 0) {
+			return false;
+		} else {
+			return $PS;
+		}
+	}
+
+	public function newReperation($customerID = null, $name, $email, $subject, $message) {
+		try {
+			$stmt = $this->conn->prepare("INSERT INTO reperations (customerID, name, email, subject, message) VALUES (:customerID, :name, :email, :subject, :message);");
+			$stmt->bindparam(":customerID",$customerID);
+			$stmt->bindparam(":name",$name);
+			$stmt->bindparam(":email",$email);
+			$stmt->bindparam(":subject",$subject);
+			$stmt->bindparam(":message",$message);
+			$stmt->execute();
+
+			return true;
+		} catch (PDOException $ex) {
+			return false;
+		}
+	}
+
+	public function updateShoppingCart($customerID, $productID) {
+		try {
+			$stmt = $this->conn->prepare("INSERT INTO shoppingcart (customerID, productID) VALUES (:customerID, :productID) ON DUPLICATE KEY UPDATE quantity = quantity + 1;");
+			$stmt->bindparam(":customerID",$customerID);
+			$stmt->bindparam(":productID",$productID);
+			$stmt->execute();
+
+			return true;
+		} catch (PDOException $ex) {
+			return false;
+		}
+	}
+
+	public function orderShoppingCart() {
+		try {
+			$shoppingCart = $this->getShoppingCart();
+			if ($shoppingCart) {
+				$stmt = $this->conn->prepare("INSERT INTO orders (customerID) VALUES (:customerID)");
+				$stmt->execute(array(":customerID"=>$_SESSION['userSession']));
+
+				$orderID = $this->lastID();
+
+				foreach ($shoppingCart as $bike) {
+					$stmt = $this->conn->prepare("INSERT INTO orderdetails (orderID, productID, quantity, priceEach) VALUES (:orderID, :productID, :quantity, :priceEach)");
+					$stmt->bindparam(":orderID",$orderID);
+					$stmt->bindparam(":productID",$bike["id"]);
+					$stmt->bindparam(":quantity",$bike["quantity"]);
+					if ($bike["isAction"]) {
+						$stmt->bindparam(":priceEach",$bike["actionPrice"]);
+					} else {
+						$stmt->bindparam(":priceEach",$bike["price"]);
+					}
+					$stmt->execute();
+				}
+	
+				return true;
+			}
+		} catch (PDOException $ex) {
+			return false;
+		}
+	}
+
+	public function getShoppingCart() {
+		$stmt = $this->conn->prepare("SELECT shoppingcart.*, products.*, IF(actionPrice IS NULL, false, true) as isAction FROM shoppingcart INNER JOIN products ON shoppingcart.productID = products.id WHERE customerID=:customerID");
+		$stmt->execute(array(":customerID"=>$_SESSION['userSession']));
+		$PS = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		if ($stmt->rowCount() == 0) {
+			return false;
+		} else {
+			return $PS;
+		}
+	}
+
+	public function emptyShoppingCart() {
+		$stmt = $this->conn->prepare("DELETE FROM shoppingcart WHERE customerID=:customerID");
+		$stmt->execute(array(":customerID"=>$_SESSION['userSession']));
+	}
+
+	public function deleteItemFromShoppingCart($cartID) {
+		try {
+			$stmt = $this->conn->prepare("DELETE FROM shoppingcart WHERE customerID=:customerID AND id=:cartID");
+			$stmt->bindparam(":customerID",$_SESSION['userSession']);
+			$stmt->bindparam(":cartID",$cartID);
+			$stmt->execute();
+
+			return true;
+		} catch (PDOException $ex) {
+			return false;
+		}
+	}
+
+	public function updateCustomerInfo($email, $firstname, $lastname, $phoneNumber, $city, $zipcode, $street, $houseNumber) {
+		try {
+			$stmt = $this->conn->prepare("UPDATE customers SET email=:email, firstname=:firstname, lastname=:lastname, phoneNumber=:phoneNumber, city=:city, zipcode=:zipcode, street=:street, houseNumber=:houseNumber WHERE id=:customerID;");
+			$stmt->bindparam(":customerID",$_SESSION['userSession']);
+			$stmt->bindparam(":email",$email);
+			$stmt->bindparam(":firstname",$firstname);
+			$stmt->bindparam(":lastname",$lastname);
+			$stmt->bindparam(":phoneNumber",$phoneNumber);
+			$stmt->bindparam(":city",$city);
+			$stmt->bindparam(":zipcode",$zipcode);
+			$stmt->bindparam(":street",$street);
+			$stmt->bindparam(":houseNumber",$houseNumber);
+			$stmt->execute();
+
+			return true;
+		} catch (PDOException $ex) {
+			return false;
+		}
+	}
+
+	public function getOrders() {
+		$stmt = $this->conn->prepare("SELECT orderdetails.orderID, SUM(orderdetails.priceEach * orderdetails.quantity) as totalPrice, SUM(orderdetails.quantity) as totalBikes, orders.orderDate FROM orderdetails INNER JOIN orders ON orderdetails.orderID = orders.id WHERE customerID=:customerID GROUP BY orderdetails.orderID");
+		$stmt->execute(array(":customerID"=>$_SESSION['userSession']));
+		$PS = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		if ($stmt->rowCount() == 0) {
+			return false;
+		} else {
+			return $PS;
+		}
+	}
+
+	public function getOrderInfo($orderID) {
+		$stmt = $this->conn->prepare("SELECT orderdetails.id, orderdetails.orderID, orders.customerID, orders.orderDate, orderdetails.productID, orderdetails.quantity, orderdetails.priceEach, products.name, products.brand, products.imagePath, customers.firstname, customers.lastname, customers.zipcode, customers.city, customers.phoneNumber, customers.street, customers.houseNumber, SUM(orderdetails.priceEach * orderdetails.quantity) as totalPrice, SUM(orderdetails.quantity) as totalBikes FROM orders INNER JOIN orderdetails ON orders.id = orderdetails.orderID INNER JOIN products ON orderdetails.productID = products.id INNER JOIN customers ON orders.customerID = customers.id WHERE orders.customerID = :customerID AND orders.id = :orderID GROUP BY orderdetails.id;");
+		$stmt->bindparam(":customerID",$_SESSION['userSession']);
+		$stmt->bindparam(":orderID",$orderID);
 		$stmt->execute();
 		$PS = $stmt->fetchAll(PDO::FETCH_ASSOC);
 		if ($stmt->rowCount() == 0) {
